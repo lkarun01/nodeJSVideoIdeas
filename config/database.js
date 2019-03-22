55dbf8307cdd4077210fa4e33817979e0d13b4da
lkarun01 <lalanke@yahoo.com>
2019-03-21 17:01:31 -0700

    db connection for mongo

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://lalanke:12345@cluster0-sgfqa.azure.mongodb.net/vidjot-prod?retryWrites=true"
+      "mongodb+srv://lalanke:12345@cluster0-sgfqa.azure.mongodb.net/test?retryWrites=true"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/vidjot-dev" };
