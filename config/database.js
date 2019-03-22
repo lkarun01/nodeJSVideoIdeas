9d87626871cb6d440c758c4388d7a4db106af84f
lkarun01 <lalanke@yahoo.com>
2019-03-21 17:51:06 -0700

    c

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://lalanke:12345@cluster0-shard-00-00-sgfqa.azure.mongodb.net:27017,cluster0-shard-00-01-sgfqa.azure.mongodb.net:27017,cluster0-shard-00-02-sgfqa.azure.mongodb.net:27017/vidjot-prod?retryWrites=true"
+      "mongodb+srv://lalanke:12345@cluster0-sgfqa.azure.mongodb.net:27017/vidjot-prod?retryWrites=true"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/vidjot-dev" };
