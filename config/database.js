2c44eb2adbef2c8ab4856a2a497da7cee4eaefe6
Lalanke <lalanke@yahoo.com>
2019-03-21 22:41:38 -0700

    solve 1

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://lalanke:12345@cluster0-sgfqa.mongodb.net/vidjot-prod?retryWrites=true"
+      "mongodb://lalanke:12345@cluster0-shard-00-00-sgfqa.mongodb.net:27017,cluster0-shard-00-01-sgfqa.mongodb.net:27017,cluster0-shard-00-02-sgfqa.mongodb.net:27017/vidjot-prod?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/vidjot-dev" };
